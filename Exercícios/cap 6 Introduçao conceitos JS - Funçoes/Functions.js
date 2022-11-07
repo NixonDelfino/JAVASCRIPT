3f8657f63f93632c110f52b4640854951617926e
NixonDelfino <116096774+NixonDelfino@users.noreply.github.com>
2022-11-06 21:34:00 -0300

    functions

---
+++ Exercícios/cap 6 Introduçao conceitos JS - Funçoes/Functions.js
@@ -0,0 +1,16 @@
+// criar um aplicativo de frases motivacionais
+
+//declaration
+//function statement
+function createPhrases() {
+    console.log('Estudar é muito bom');
+    console.log('Paciência e presistência');
+    console.log('Revisão é a mãe do aprendizado');
+}
+
+//executar a função
+//execute, run, call, invoke
+
+createPhrases()
+createPhrases()
+createPhrases()
---
+++ Exercícios/cap 6 Introduçao conceitos JS - Funçoes/JSbase.js
@@ -0,0 +1,12 @@
+// function expression
+// Function anonymous
+
+//parameter
+const sum = function(number1, number2) {
+    console.log(number1 + number2);
+}
+
+sum(2, 3)
+sum(9, 5) //arguments
+
+CAP 6 aula 03
