ae1907a62809814e832d15c52e3242119791afa5
NixonDelfino <116096774+NixonDelfino@users.noreply.github.com>
2022-11-13 11:05:27 -0300

    Começando cap 8 JS

---
+++ Exercícios/5.Introdução aos conceitos JS/cap 8 expressões e operadores/aula 1.js
@@ -0,0 +1,17 @@
+/* Expressões e operadores
+
+-Expressions
+-Operators
+    Binary
+    Unary
+    ternary
+*/
+
+let number = 1 //Expression
+
+console.log(number + 1); //operador binário
+
+console.log(--number); //operador unário
+
+console.log(false ? 'alo' : 'nada'); //operador ternário (único)
+
