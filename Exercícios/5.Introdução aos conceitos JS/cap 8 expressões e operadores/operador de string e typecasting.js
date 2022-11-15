171d78d9522a96078377dbb3ab9ac2e6d75b2521
NixonDelfino <116096774+NixonDelfino@users.noreply.github.com>
2022-11-15 13:16:20 -0300

    Cap 8 Finalizado

---
+++ Exercícios/5.Introdução aos conceitos JS/cap 8 expressões e operadores/operador de string e typecasting.js
@@ -0,0 +1,47 @@
+// Operador de String (string operator)
+
+//comparison (comparação)
+//console.log('a' == 'b');
+
+//concatenation (concatenação)
+//retorna a união de duas strings
+/*
+let alpha = 'alpha'
+console.log(alpha + 'bet' + 123);
+*/
+
+//Type cpnversion (typecasting) vs Type coersion
+
+//console.log(Number ('9') + 5);
+
+/*
+FALSY
+Quando um valor é considerado false em contextos onde um booleano é obrigatório (condicionais e loops)
+
+false
+0
+-0
+""
+null
+undefined
+NaN
+*/
+
+console.log(0 ? 'verdadeiro' : 'falso');
+
+/*
+TRUTHY
+Quando um valorn é considerado true em contextos onde um booleano é obrigatório (condicionais e loops)
+
+true
+{}
+[]
+1
+3.23
+"0"
+"false"
+-1
+Infinity
+-Infinity
+*/
+console.log(true ? 'Verdadeiro' : 'Falso');
