02de53cbb98f5666403c28812adae97bbc164541
NixonDelfino <116096774+NixonDelfino@users.noreply.github.com>
2022-12-07 12:49:18 -0300

    termino esmodules explicação

--- Exercícios/7 . Avançando no JS/esmodules/main.js
+++ Exercícios/7 . Avançando no JS/esmodules/main.js
@@ -1,1 +1,15 @@
-import './utils.js'
+//import qualquernome from './utils.js' // com default
+
+//import {message, anotherMessage} from "./utils.js";
+
+//console.log(message, anotherMessage);
+//import as
+/*import {sum as soma} from './utils.js'
+
+console.log(soma(3, 2))*/
+
+//importando multiplas
+
+import * as calculator from './utils.js'
+
+console.log(calculator.divide(1, 3));
--- Exercícios/7 . Avançando no JS/esmodules/utils.js
+++ Exercícios/7 . Avançando no JS/esmodules/utils.js
@@ -1,2 +1,20 @@
-const message = "message"
-alert(message)
+//export const message = "mensagem" //export direto individual
+//const anotherMessage = "Outra mensagem"
+//alert(message)
+
+// export default message //apenas 1 default por arquivo
+
+//export {anotherMessage} //export por lista
+
+//da para usar os dois ao mesmo tempo
+
+// export as
+/*export function sum(a, b) {
+    return a + b
+}*/
+//exportando multplas
+
+export const sum = (a, b) => a + b
+export const multiply = (a, b) => a * b
+export const subtract = (a, b) => a - b
+export const divide = (a, b) => a / b
