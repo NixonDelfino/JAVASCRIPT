c13ce936e38343e02e60b7f5a7b4ef82ea6f2a9b
NixonDelfino <116096774+NixonDelfino@users.noreply.github.com>
2022-12-08 00:06:36 -0300

    Avançando  no projeto timer

--- Exercícios/7 . Avançando no JS/cronômetro/js/main.js
+++ Exercícios/7 . Avançando no JS/cronômetro/js/main.js
@@ -4,12 +4,17 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+let minutes
 
 function playPauseButtonChange(event) {
     buttonPlay.classList.toggle('hide')
     buttonPause.classList.toggle('hide')
     buttonStop.classList.remove('hide')
     buttonSet.classList.add('hide')
+
+    setTimeout(function () {secondsDisplay.textContent = Number(secondsDisplay.textContent) - 1}, 1000)
 }
 function stopApplication(event) {
     buttonPlay.classList.remove('hide')
@@ -21,11 +26,14 @@ function toggleSound(event) {
     buttonSoundOn.classList.toggle('hide')
     buttonSoundOff.classList.toggle('hide')
 }
+function changeTimerValue(event) {
+    minutes = prompt('Digite a quantidade de minutos para o cronômetro')
+    minutesDisplay.textContent = minutes
+}
 
 buttonPlay.addEventListener('click', playPauseButtonChange)
 buttonPause.addEventListener('click', playPauseButtonChange)
 buttonStop.addEventListener('click', stopApplication)
 buttonSoundOn.addEventListener('click', toggleSound)
 buttonSoundOff.addEventListener('click', toggleSound)
-
-aula9
+buttonSet.addEventListener('click', changeTimerValue)
